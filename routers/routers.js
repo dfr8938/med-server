daffdd9e9ce6d2555f6359714f2ba968e7a87b9f
dfr8938 <d.fr8938@gmail.com>
2024-11-04 19:08:55 +0300

    update server

--- routers/routers.js
+++ routers/routers.js
@@ -1,10 +1,8 @@
 import { Router } from "express";
 const routers = new Router();
 
-import {anatRouter} from "./anatRouter.js";
 import {patRouter} from "./patRouter.js";
 
-routers.use("/", anatRouter);
 routers.use("/", patRouter);
 
 export { routers };
--- utils/consts.js
+++ utils/consts.js
@@ -1,8 +1,6 @@
 const BASIC_URL = "/";
 const ID_URL = "/:id";
 
-const ANAT_URL = "/anat";
-
 const PAT_URL = "/pat";
 
-export { BASIC_URL, ID_URL, ANAT_URL, PAT_URL };
+export { BASIC_URL, ID_URL, PAT_URL };
